4480f1b19e2f1583ce54ca75c6ae905bb3c32b94
Andy Wermke <andy.dev@next-step-software.com>
2018-12-23 14:50:45 +0100

    Refactor templating logic

--- src/sql.ts
+++ src/sql.ts
@@ -1,42 +1,156 @@
-export interface SqlRawExpressionValue {
-  type: symbol
-  subtype: symbol
-  rawValue: string
+import createDebugLogger from "debug"
+import { PgQueryConfig } from "./types"
+
+export interface QueryConfig {
+  text: string
+  values: any[]
 }
 
-export interface SqlAndSpread<RecordType> {
+interface SqlSpecialExpressionValue {
   type: symbol
-  subtype: symbol
-  record: RecordType
+  buildFragment(nextParamID: number): QueryConfig
 }
 
-export interface SqlInsertValuesSpread<RecordType> {
-  type: symbol
-  subtype: symbol
-  record: RecordType
-}
+const debugQuery = createDebugLogger("sqldb:query")
 
-export type SqlSpecialExpressionValue = SqlInsertValuesSpread<any> | SqlRawExpressionValue
+const $sqlExpressionValue = Symbol("SQL expression value")
 
-export const $sqlExpressionValue = Symbol("SQL expression value")
-export const $sqlSpreadAnd = Symbol("SQL AND spread")
-export const $sqlSpreadInsert = Symbol("SQL INSERT values")
-export const $sqlRawExpressionValue = Symbol("SQL raw expression value")
-
-export const isInsertValuesExpression = (
-  expression: SqlSpecialExpressionValue
-): expression is SqlInsertValuesSpread<any> =>
-  expression.type === $sqlExpressionValue && expression.subtype === $sqlSpreadInsert
-
-export const isAndSpreadExpression = (
-  expression: SqlSpecialExpressionValue
-): expression is SqlInsertValuesSpread<any> =>
-  expression.type === $sqlExpressionValue && expression.subtype === $sqlSpreadAnd
-
-export const isRawExpressionValue = (
-  expression: SqlSpecialExpressionValue
-): expression is SqlRawExpressionValue =>
-  expression.type === $sqlExpressionValue && expression.subtype === $sqlRawExpressionValue
-
-export const isSpecialExpression = (expression: any): expression is SqlSpecialExpressionValue =>
+const isSpecialExpression = (expression: any): expression is SqlSpecialExpressionValue =>
   expression && typeof expression === "object" && expression.type === $sqlExpressionValue
+
+function escapeIdentifier(identifier: string) {
+  if (identifier.charAt(0) === '"' && identifier.charAt(identifier.length - 1) === '"') {
+    identifier = identifier.substr(1, identifier.length - 2)
+  }
+  if (identifier.includes('"')) {
+    throw new Error(`Invalid identifier: ${identifier}`)
+  }
+  return `"${identifier}"`
+}
+
+function objectEntries<T extends { [key: string]: Value }, Value = any>(
+  object: T
+): Array<[string, Value]> {
+  const keys = Object.keys(object)
+  return keys.map(key => [key, object[key]] as [string, Value])
+}
+
+function serializeSqlTemplateExpression(expression: any, nextParamID: number): QueryConfig {
+  const values: any[] = []
+
+  if (isSpecialExpression(expression)) {
+    return expression.buildFragment(nextParamID)
+  } else {
+    const text = pushSqlParameter(nextParamID, values, expression)
+    return {
+      text,
+      values
+    }
+  }
+}
+
+export function sql(texts: TemplateStringsArray, ...values: any[]): PgQueryConfig {
+  let parameterValues: any[] = []
+  let resultingSqlQuery = texts[0]
+
+  for (let valueIndex = 0; valueIndex < values.length; valueIndex++) {
+    const expression = values[valueIndex]
+    const followingSqlText = texts[valueIndex + 1] as string | undefined
+    const nextParamID = parameterValues.length + 1
+
+    const serialized = serializeSqlTemplateExpression(expression, nextParamID)
+    resultingSqlQuery += serialized.text + (followingSqlText || "")
+
+    parameterValues = [...parameterValues, ...serialized.values]
+  }
+
+  const query = {
+    text: resultingSqlQuery,
+    values: parameterValues
+  }
+
+  debugQuery(query)
+  return query
+}
+
+function rawExpression(rawValue: string): SqlSpecialExpressionValue {
+  return {
+    type: $sqlExpressionValue,
+    buildFragment() {
+      return {
+        text: rawValue,
+        values: []
+      }
+    }
+  }
+}
+
+export { rawExpression as raw }
+
+sql.raw = rawExpression
+
+function pushSqlParameter(nextParamID: number, parameterValues: any[], value: any) {
+  parameterValues.push(value)
+  return `\$${nextParamID}`
+}
+
+function buildSpreadAndFragment(
+  columnValues: Array<[string, any]>,
+  nextParamID: number
+): QueryConfig {
+  let values: any[] = []
+  const andChain = columnValues
+    .map(([columnName, columnValue]) => {
+      const serialized = serializeSqlTemplateExpression(columnValue, nextParamID)
+      values = [...values, ...serialized.values]
+      nextParamID += serialized.values.length
+      return `${escapeIdentifier(columnName)} = ${serialized.text}`
+    })
+    .join(" AND ")
+  return {
+    text: `(${andChain})`,
+    values
+  }
+}
+
+function buildSpreadInsertFragment(
+  insertionValues: Array<[string, any]>,
+  nextParamID: number
+): QueryConfig {
+  let values: any[] = []
+  const text =
+    `(${insertionValues.map(([columnName]) => escapeIdentifier(columnName)).join(", ")})` +
+    ` VALUES ` +
+    `(${insertionValues
+      .map(([, columnValue]) => {
+        const serialized = serializeSqlTemplateExpression(columnValue, nextParamID)
+        values = [...values, ...serialized.values]
+        nextParamID += serialized.values.length
+        return serialized.text
+      })
+      .join(", ")})`
+  return {
+    text,
+    values
+  }
+}
+
+export function spreadAnd(record: any): SqlSpecialExpressionValue {
+  const values = objectEntries<any>(record)
+  return {
+    type: $sqlExpressionValue,
+    buildFragment(nextParamID: number) {
+      return buildSpreadAndFragment(values, nextParamID)
+    }
+  }
+}
+
+export function spreadInsert<RecordType>(record: RecordType): SqlSpecialExpressionValue {
+  const insertionValues = objectEntries<any>(record)
+  return {
+    type: $sqlExpressionValue,
+    buildFragment(nextParamID: number) {
+      return buildSpreadInsertFragment(insertionValues, nextParamID)
+    }
+  }
+}
--- test/query.test.ts
+++ test/query.test.ts
@@ -1,5 +1,11 @@
 import test from "ava"
-import { spreadAnd, spreadInsert, sql } from "../src/pg"
+import dedent from "dedent"
+import { spreadAnd, spreadInsert, sql, QueryConfig } from "../src/pg"
+
+const dedentQueryConfig = (queryConfig: QueryConfig) => ({
+  ...queryConfig,
+  text: dedent(queryConfig.text)
+})
 
 test("sql template tag creates a valid postgres query", t => {
   t.deepEqual(sql`SELECT email FROM users WHERE id = ${1}`, {
@@ -16,15 +22,58 @@ test("sql.raw() works", t => {
 })
 
 test("spreadAnd() works", t => {
-  t.deepEqual(sql`SELECT * FROM users WHERE ${spreadAnd({ name: "Hugo", age: 20 })}`, {
-    text: `SELECT * FROM users WHERE ("name" = $1 AND "age" = $2)`,
-    values: ["Hugo", 20]
-  })
+  t.deepEqual(
+    sql`
+    SELECT * FROM users WHERE ${spreadAnd({
+      name: "Hugo",
+      age: 20,
+      email: sql.raw("'foo@example.com'")
+    })}
+  `,
+    {
+      text: `SELECT * FROM users WHERE ("name" = $1 AND "age" = $2 AND "email" = 'foo@example.com')`,
+      values: ["Hugo", 20]
+    }
+  )
 })
 
 test("spreadInsert() works", t => {
-  t.deepEqual(sql`INSERT INTO users ${spreadInsert({ name: "Hugo", age: 20 })} RETURNING *`, {
-    text: `INSERT INTO users ("name", "age") VALUES ($1, $2) RETURNING *`,
-    values: ["Hugo", 20]
-  })
+  t.deepEqual(
+    sql`
+    INSERT INTO users ${spreadInsert({
+      name: "Hugo",
+      age: 20,
+      created_at: sql.raw("NOW()")
+    })} RETURNING *
+  `,
+    {
+      text: `INSERT INTO users ("name", "age", "created_at") VALUES ($1, $2, NOW()) RETURNING *`,
+      values: ["Hugo", 20]
+    }
+  )
+})
+
+test("works with multiple parameters", t => {
+  t.deepEqual(
+    dedentQueryConfig(sql`
+    WITH some_users AS (
+      SELECT * FROM users WHERE id = ${1} OR email = ${"foo@example.com"}
+    )
+    INSERT INTO users ${spreadInsert({
+      name: "Hugo",
+      age: 20,
+      created_at: sql.raw("NOW()"),
+      role: "user"
+    })} UNION SELECT * FROM some_users RETURNING *
+  `),
+    dedentQueryConfig({
+      text: `
+    WITH some_users AS (
+      SELECT * FROM users WHERE id = $1 OR email = $2
+    )
+    INSERT INTO users ("name", "age", "created_at", "role") VALUES ($3, $4, NOW(), $5) UNION SELECT * FROM some_users RETURNING *
+    `,
+      values: [1, "foo@example.com", "Hugo", 20, "user"]
+    })
+  )
 })

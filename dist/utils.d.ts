13b61e98865fa415940ec8d12b64399d45e1d9ab
Brandon Chinn <brandon@leapyear.io>
2020-05-03 13:46:53 -0700

    Update dist

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -4,9 +4,9 @@
 export declare function escapeIdentifier(identifier: string): string;
 export declare type ValueRecord<T = any> = Record<string, T>;
 /**
- * Get the entries of the given object, filtering out undefined values.
+ * Filter out all undefined keys in the given record.
  */
-export declare function objectEntries<Value = any>(object: ValueRecord<Value>): Array<[string, Value]>;
+export declare function filterUndefined<Value = any>(object: ValueRecord<Value>): ValueRecord<Value>;
 /**
  * Merge two lists, alternating between elements.
  *
@@ -14,3 +14,8 @@ export declare function objectEntries<Value = any>(object: ValueRecord<Value>): 
  * mergeLists([x1, x2, x3], [y1, y2, y3]) == [x1, y1, x2, y2, x3, y3]
  */
 export declare function mergeLists<T>(list1: ReadonlyArray<T>, list2: ReadonlyArray<T>): T[];
+/**
+ * Get the keys from the given list of objects. Checks that the objects all
+ * have the same keys.
+ */
+export declare function extractKeys<Value = any>(objects: Array<ValueRecord<Value>>): string[];
